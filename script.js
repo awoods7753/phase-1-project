a55c3989da4884ce462aa30f6cac91918e8593d5
Austin <awoods7753@gmail.com>
2022-03-25 13:06:48 -0400

    DOM Card styling, Vote button on characters

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ function addOneCharacter(characters) {
     let characterContainer = document.querySelector('#characters-container')
     let card = document.createElement('div')
     card.id = "character-card"
-    let name = document.createElement('h2')
+    let name = document.createElement('h1')
     name.innerText = characters.name
     let img = document.createElement('img')
     img.src = characters.image
-    let description = document.createElement('p')
-    description.innerText = characters.description
+    let description = document.createElement('h4')
+    description.innerText = `Bio: ${characters.description}`
+    let likebutton = document.createElement('button')
+    likebutton.innerHTML = 'Vote! By Order of the Peaky Blinders!'
+    likebutton.id = "like-button"
 
-    card.append(name, img, description)
+    card.append(name, img, description, likebutton)
     characterContainer.append(card)
 }
