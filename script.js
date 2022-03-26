efd9a5c516aec47c5dae4e43b2f40df794c8717c
Austin <awoods7753@gmail.com>
2022-03-26 14:02:35 -0400

    Changes to script.js - added // code to update the db.json with vote information

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Initial function to fetch data from db.json for the characters
 let fetchCharacters = () => {
     fetch('http://localhost:3000/characters/?_limit=5&_page=1')
     .then(resp=>resp.json())
@@ -10,6 +11,7 @@ let fetchCharacters = () => {
 }
 fetchCharacters();
 
+//function to add the characters and their information to the character card on the DOM
 function addOneCharacter(characters) {
     let characterContainer = document.querySelector('#characters-container')
     let card = document.createElement('div')
@@ -20,10 +22,45 @@ function addOneCharacter(characters) {
     img.src = characters.image
     let description = document.createElement('h4')
     description.innerText = `Bio: ${characters.description}`
+
     let likebutton = document.createElement('button')
     likebutton.innerHTML = 'Vote! By Order of the Peaky Blinders!'
     likebutton.id = "like-button"
+    likebutton.addEventListener('click', (e) => addAVote(e))
 
-    card.append(name, img, description, likebutton)
+    let votes = document.createElement('h5')
+    votes.innerText = `Votes: ${characters.votes}`
+    votes.id = "votes-id"
+
+    card.append(name, img, description, likebutton, votes)
     characterContainer.append(card)
 }
+//Adding the ability for the vote button to increment the db.json votes #
+
+// let addAVote = (characters) => {
+//     console.log(characters)
+//     fetch('http://localhost:3000/characters', {
+//         method: "PATCH",
+//         headers: 
+//              {
+//                  "Content-Type":"application/json",
+//                  Accept: "application/json"
+//              },
+//         body: JSON.stringify(
+//             characters.votes
+//         )
+//     })
+//     .then(resp => resp.json())
+//     .then(votes => {
+//         incrementVotes(votes)
+//     })
+// }
+
+// function incrementVotes(characters) {
+//     let votesNumber = characters.votes
+//     let votesContainer = document.getElementById('votes-id')
+//     votesContainer.value = votesNumber
+//     votesNumber++
+// }
+
+//Function to add the submit form to the bottom-reel of the website
